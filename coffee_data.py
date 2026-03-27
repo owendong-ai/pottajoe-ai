e9d3e037501c073f63b37a563d80a29fcc728a82
barrywei7451 <owendong7451@gmail.com>
2026-03-27 02:31:23 -0700

    增加咖啡種類的圖片

--- coffee_data.py
+++ coffee_data.py
@@ -16,19 +16,19 @@ coffees = [
         "name": "PottaJoe 義式濃縮",
         "flavor": "濃郁",
         "description": "純粹義式風味，濃烈直接，帶有奶油般的脂層",
-        "image": "images/classic_dark.png"
+        "image": "images/espresso.jpg"
     },
     {
         "name": "PottaJoe 摩卡濃醇",
         "flavor": "濃郁",
         "description": "濃郁咖啡與巧克力完美融合，層次豐富迷人",
-        "image": "images/deep_roast.png"
+        "image": "images/mocha.jpg"
     },
     {
         "name": "PottaJoe 黑金特選",
         "flavor": "濃郁",
         "description": "嚴選高海拔豆，深焙帶出濃厚焦糖甜感",
-        "image": "images/classic_dark.png"
+        "image": "images/black_gold.jpg"
     },
 
     # 果香
@@ -48,19 +48,19 @@ coffees = [
         "name": "PottaJoe 衣索比亞日曬",
         "flavor": "果香",
         "description": "藍莓與覆盆子的天然果香，酸甜迷人",
-        "image": "images/fruity_fresh.png"
+        "image": "images/ethiopia.jpg"
     },
     {
         "name": "PottaJoe 肯亞精選",
         "flavor": "果香",
         "description": "黑醋栗與番茄風味交織，酸質明亮活潑",
-        "image": "images/morning_fruit.png"
+        "image": "images/kenya.jpg"
     },
     {
         "name": "PottaJoe 哥倫比亞水洗",
         "flavor": "果香",
         "description": "蘋果與柑橘調，清爽均衡，後韻甜美",
-        "image": "images/fruity_fresh.png"
+        "image": "images/colombia.jpg"
     },
 
     # 堅果
@@ -80,19 +80,19 @@ coffees = [
         "name": "PottaJoe 曼特寧",
         "flavor": "堅果",
         "description": "低酸厚實，帶有核桃與黑糖香，餘韻悠長",
-        "image": "images/nutty_smooth.png"
+        "image": "images/mandheling.jpg"
     },
     {
         "name": "PottaJoe 巴西丘比",
         "flavor": "堅果",
         "description": "溫潤的花生與牛奶巧克力風味，口感柔順",
-        "image": "images/hazelnut_latte.png"
+        "image": "images/brazil.jpg"
     },
     {
         "name": "PottaJoe 瓜地馬拉",
         "flavor": "堅果",
         "description": "杏仁與奶油糖香，口感滑順細膩",
-        "image": "images/nutty_smooth.png"
+        "image": "images/guatemala.jpg"
     },
 
     # 花香
@@ -112,18 +112,18 @@ coffees = [
         "name": "PottaJoe 巴拿馬藝妓",
         "flavor": "花香",
         "description": "極致花香與柑橘調，細緻優雅，咖啡界頂級珍品",
-        "image": "images/floral_soft.png"
+        "image": "images/geisha.jpg"
     },
     {
         "name": "PottaJoe 耶加雪菲",
         "flavor": "花香",
         "description": "茉莉花香與檸檬草調，清新脫俗令人著迷",
-        "image": "images/spring_flower.png"
+        "image": "images/yirgacheffe.jpg"
     },
     {
         "name": "PottaJoe 瑰夏珍品",
         "flavor": "花香",
         "description": "玫瑰與水蜜桃香氣，甜美迷人，風味層次豐富",
-        "image": "images/floral_soft.png"
+        "image": "images/guaixa.jpg"
     },
 ]
